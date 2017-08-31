7c9298171c04d90cabe59e00d9fb0cbc1702716e
Keaton Foster <tkfoster26@gmail.com>
2017-08-31 12:29:39 -0400

    chromedriver installed, using older version of cucumber

---
+++ selenium/features/login_logout.feature
@@ -0,0 +1,8 @@
+Feature: Toggling Sound
+
+Background:
+  Given I navigate to "https://dev.dev-rhinogram.com"
+ 
+Scenario: Toggle Off and Save
+  Given I login with "tonton" and "chacoz"
+  Then I logout
--- selenium/features/step_definitions/step_defs.js
+++ selenium/features/step_definitions/step_defs.js
@@ -4,13 +4,12 @@ const selenium = require('selenium-webdriver');
 
 const by  = selenium.By;
 
-module.exports = function cuke() {
-  const driver = new sel.Builder().forBrowser('chrome').setScrollBehavior(0).build();
+module.exports = function() {
+  const driver = new selenium.Builder().forBrowser('chrome').setScrollBehavior(0).build();
 
   this.setDefaultTimeout(13000);
 
-  this.Then(/^pre-login-setup$/, (callback) => {
-    uni.getProfile(driver);
-    setTimeout(() => uni.logoutIfPossible(driver, by, callback), 2000);
+  this.Then(/^I navigate to "([^"]*)"$/, (url, callback) => {
+    driver.get(url);
   });
 };

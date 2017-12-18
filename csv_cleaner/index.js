dbd61078053603380ea6606211222ebcb205e5fe
Keaton Foster <tkfoster26@gmail.com>
2017-12-18 12:04:17 -0500

    filtering out matching externalIds

--- csv_cleaner/index.js
+++ csv_cleaner/index.js
@@ -1,16 +1,32 @@
-const csv = require('csv');
+// const csv = require('csv');
 const fs = require('fs');
+const csvtojson = require('csvtojson');
+const csvFile = process.env.CSVFILE;
+const acceptableFields = ['firstName', 'middleName', 'lastName', 'preferredName', 'birthday', 'email', 'homePhone', 'cellPhone', 'workPhone', 'phone', 'sex', 'externalId'];
 
-let csvData;
-fs.readFile('./sample_csv/oneguy.csv', function(err, data) {
-  if (err) {
-    return console.error(err);
-  }
-  csvData = data.toString();
-  console.log(csvData);
-  csv.parse(csvData, {comment: '#'}, function (err, output) {
-    console.log('OUTPUT', output);
-  });
-});
+const filePath = `./sample_csv/${csvFile}.csv`;
 
-console.log('CSV', csv);
+function runCsvParser() {
+  let objArray = [];
+  csvtojson()
+    .fromFile(filePath)
+    .on('json', (jsonObj) => {
+      let currentExternalId = jsonObj.externalId;
+      for (let i = 0; i <= objArray.length; i++) {
+        // filters out entries with duplicate external IDs
+        if (objArray.length > i && objArray[i].externalId === currentExternalId) {
+          objArray.splice(i, 1);
+        }
+      }
+      objArray.push(jsonObj);
+    })
+    .on('done', (err) => {
+      if (err) {
+        console.log('ERROR: ', err);
+      }
+      console.log('SUCCESS: ', objArray);
+      return objArray;
+    });
+}
+
+runCsvParser();

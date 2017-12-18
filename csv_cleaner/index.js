1b77e98194a027ea325f7b2c7ee102e109689071
Keaton Foster <tkfoster26@gmail.com>
2017-12-18 16:24:47 -0500

    holy shit

--- csv_cleaner/index.js
+++ csv_cleaner/index.js
@@ -7,26 +7,57 @@ const acceptableFields = ['firstName', 'middleName', 'lastName', 'preferredName'
 const filePath = `./sample_csv/${csvFile}.csv`;
 
 function runCsvParser() {
-  let objArray = [];
+  const cleanedUserObjects = [];
+  const objectsWithDuplicateExternalIds = [];
   csvtojson()
     .fromFile(filePath)
     .on('json', (jsonObj) => {
       let currentExternalId = jsonObj.externalId;
-      for (let i = 0; i <= objArray.length; i++) {
+      for (let i = 0; i <= cleanedUserObjects.length; i++) {
         // filters out entries with duplicate external IDs
-        if (objArray.length > i && objArray[i].externalId === currentExternalId) {
-          objArray.splice(i, 1);
+        if (cleanedUserObjects.length > i && cleanedUserObjects[i].externalId === currentExternalId) {
+          // add the phone of the to-be-removed user object to the one being saved
+          if (jsonObj.phone !== '') {
+            consolidatePhones(jsonObj, cleanedUserObjects[i]);          
+          }
+          // remove the matching user so they don't exist more than once
+          cleanedUserObjects.splice(i, 1);
+          // add to array of duplicates
+          objectsWithDuplicateExternalIds.push(jsonObj);
         }
       }
-      objArray.push(jsonObj);
+      cleanedUserObjects.push(jsonObj);
     })
     .on('done', (err) => {
       if (err) {
         console.log('ERROR: ', err);
       }
-      console.log('SUCCESS: ', objArray);
-      return objArray;
+      console.log('SUCCESS: ', cleanedUserObjects);
+      return {
+        clean: cleanedUserObjects,
+        duplicates: objectsWithDuplicateExternalIds
+      }
     });
+}
+
+function consolidatePhones(newObj, existingObj) {
+  console.log('existing: ', existingObj);
+  if (existingObj.phone === '') {
+    console.log('44--------');
+    return newObj;
+  } else if (existingObj.phone !== '' && existingObj.homePhone === '') {
+    console.log('46---------');
+    newObj.homePhone = existingObj.phone;
+  } else if (existingObj.phone !== '' && existingObj.homePhone !== '' && existingObj.workPhone === '') {
+    console.log('49---------');
+    newObj.workPhone = existingObj.phone;
+    newObj.homePhone = existingObj.homePhone;
+  } else if (existingObj.phone !== '' && existingObj.homePhone !== '' && existingObj.workPhone !== '' && existingObj.cellPhone === '') {
+    console.log('52---------');
+    newObj.cellPhone = existingObj.phone
+    newObj.homePhone = existingObj.homePhone;
+    newObj.workPhone = existingObj.workPhone;
+  }
 }
 
 runCsvParser();

4f935b85cce6da713adc50f81e531dc0e079ed50
geoffmaas <geoff@rhinogram.com>
2018-05-30 16:13:33 -0400

    added availability hours

--- nightwatch_tests_RG3/page_objects/ChannelsEditPage.js
+++ nightwatch_tests_RG3/page_objects/ChannelsEditPage.js
@@ -105,95 +105,11 @@ module.exports = {
         },
 
         /*-----------------------------------------------------*/
-        // Availability hours
-        /*-----------------------------------------------------*/
-
-        availabilityHoursToggle: {
-            selector: `(//LABEL[@class='rhinoswitcher__label'])[1]`, //id could be here too
-            locateStrategy: 'xpath'
-        },
-
-        sundayFrom: {
-            selector: `//SELECT[contains(@id,'from-0')]`,
-            locateStrategy: 'xpath'
-        },
-
-        sundayTo: {
-            selector: `//SELECT[contains(@id,'from-0')]`,
-            locateStrategy: 'xpath'
-        },
-
-        mondayFrom: {
-            selector: `//SELECT[contains(@id,'from-1')]`,
-            locateStrategy: 'xpath'
-        },
-
-        mondayTo: {
-            selector: `//SELECT[contains(@id,'to-1')]`,
-            locateStrategy: 'xpath'
-        },
-
-        tuesdayFrom: {
-            selector: `//SELECT[contains(@id,'from-2')]`,
-            locateStrategy: 'xpath'
-        },
-
-        tuesdayTo: {
-            selector: `//SELECT[contains(@id,'to-2')]`,
-            locateStrategy: 'xpath'
-        },
-
-        wednesdayFrom: {
-            selector: `//SELECT[contains(@id,'from-3')]`,
-            locateStrategy: 'xpath'
-        },
-
-        wednesdayTo: {
-            selector: `//SELECT[contains(@id,'to-3')]`,
-            locateStrategy: 'xpath'
-        },
-
-        thursdayFrom: {
-            selector: `//SELECT[contains(@id,'from-4')]`,
-            locateStrategy: 'xpath'
-        },
-
-        thursdayTo: {
-            selector: `//SELECT[contains(@id,'to-4')]`,
-            locateStrategy: 'xpath'
-        },
-
-        fridayFrom: {
-            selector: `//SELECT[contains(@id,'from-5')]`,
-            locateStrategy: 'xpath'
-        },
-
-        fridayTo: {
-            selector: `//SELECT[contains(@id,'to-5')]`,
-            locateStrategy: 'xpath'
-        },
-
-        saturdayFrom: {
-            selector: `//SELECT[contains(@id,'from-6')]`,
-            locateStrategy: 'xpath'
-        },
-
-        saturdayTo: {
-            selector: `//SELECT[contains(@id,'to-6')]`,
-            locateStrategy: 'xpath'
-        },
-
-        autoResponseTextArea: {
-            selector: `//TEXTAREA[contains(@id,'autoResponse')]`,
-            locateStrategy: 'xpath'
-        },
-
-        /*-----------------------------------------------------*/
         // Channel forwarding
         /*-----------------------------------------------------*/
 
         channelForwardingToggle: {
-            selector: `(//LABEL[@class='rhinoswitcher__label'])[2]`, // id could be used here
+            selector: `//LABEL[contains(@for,'allowChannelReroute')]`, // id could be used here
             locateStrategy: 'xpath'
         }
     }

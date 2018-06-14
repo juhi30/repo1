000fb332d85e9f5868ffdbd7d2ae69a1af7f1d26
Keaton Foster <tkfoster26@gmail.com>
2018-06-14 17:18:48 -0400

    capitalized TagsPage, added some steps to messaging demo

--- nightwatch_tests/tests/endUserMessaging.demo.js
+++ nightwatch_tests/tests/endUserMessaging.demo.js
@@ -2,22 +2,36 @@
 // using the Page Objects and show some of the capabilities of 
 // Selenium / Nightwatch.
 
+const helpers = require('../helpers');
+let num = helpers.randoNum;
+
 // Nightwatch has some issues with the ES6 method of exporting modules,
 // so we still do this the ES5 way for now. 
-
 module.exports = {
   /*
     Nightwatch passes in the "client" object into each test file. This object contains all of
     the Page Objects. These "page objects" contain the functions we'll want to use for each test we write. 
     Generally speaking, we always start at the Login page.
   */
-  'Login Page with Correct Credentials': function(client) {
+  'Login as a patient': function(client) {
     // this login object contains functions for any action we would want to
     // take, also any elements we want to interact with, on the Login page.
     const login = client.page.LoginPage();
 
+    // navigate() and submit() are native Nightwatch functions, where as enterPatientCreds() is one we wrote ourselves in the LoginPage.js page object.
+    // PageObjects will always have access to both. 
+    
     login.navigate()
       .enterPatientCreds('kvothe', 'Kingkiller1!')
       .submit()
   },
+
+  'Send a message as a patient': function(client) {
+    const endUserThread = client.page.EUThreadPage();
+    
+    endUserThread.fillInMessageInput(`Just a test ${num}`)
+      .clickSend()
+      .clickSettingsDropdown()
+      .clickLogoutButton();
+  }
 }

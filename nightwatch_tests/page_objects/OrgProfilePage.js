95e6122fd01e49c9d60ddd78c1b0f19253b5e955
geoffmaas <geoff@rhinogram.com>
2018-06-26 15:25:46 -0400

    added custom-commands folder

--- nightwatch_tests/page_objects/OrgProfilePage.js
+++ nightwatch_tests/page_objects/OrgProfilePage.js
@@ -4,54 +4,6 @@ const orgProfileCommands = {
     this.api.pause(time);
     return this;
   },
-
-  renderPageElements: function() {
-    return this.waitForElementVisible('@addLogoButton', 5000, 'Add logo button is visible')
-      .verify.visible('@orgNameInput', 'Name input is visible')
-      .verify.visible('@addressOneInput', 'Address input is visible')
-      .verify.visible('@cityInput', 'City input is visible')
-      .verify.visible('@stateInput', 'State input is visible')
-      .verify.visible('@zipInput', 'Zip Input is visible')
-      .verify.visible('@orgPhoneInput', 'Org phone input is visible')
-      .verify.visible('@orgEmailInput', 'Org email input is visible')
-      .verify.visible('@orgContactNameInput', 'Contact name is visible')
-      .verify.visible('@orgContactPhoneInput', 'Contact number is visible')
-      .verify.visible('@orgEmailInput', 'Contact email is visible')
-      .verify.visible('@saveOrgProfileButton', 'Save profile button is visible')
-  },
-
-  renderUploadPhotoPopup: function() {
-    return this.click('@addLogoButton')
-      .waitForElementVisible('@uploadPhotoButton', 5000, 'Upload photo popup is visible')
-      .click('@closeUploadPhotoIcon')
-      .waitForElementNotVisible('@uploadPhotoButton', 5000, 'Upload photo popup is hidden')
-  },
-
-  clearPrefilledValues: function() {
-    return this.waitForElementVisible('@orgNameInput', 'Name input visible ready to be cleared')
-      .clearValue('@orgNameInput')
-      .clearValue('@addressOneInput')
-      .clearValue('@cityInput')
-  },
-
-  setNewValues: function(name, address, city) {
-    return this.waitForElementVisible('@orgNameInput', 'Name input is visible ready to set new values')
-      .setValue('@orgNameInput', name)
-      .setValue('@addressOneInput', address)
-      .setValue('@cityInput', city)
-  },
-
-  clickSaveProfile: function() {
-    return this.click('@saveOrgProfileButton')
-  },
-
-  renderValidators: function() {
-    return this.waitForElementVisible('@alertBox', 5000, 'Alert box is visible')
-  },
-
-  validateSaveToast: function() {
-    return this.waitForElementVisible('@saveToast', 5000, 'Save toast is visible')
-  },
 }
 
 module.exports = {
@@ -132,35 +84,35 @@ module.exports = {
 
     /*----------------------------------------------------------------------------*/
 
-    nullNameInput: {
-      selector: `//DIV[@class='form__validation-message'][text()='Name is required']`,
-      locateStrategy: 'xpath',
-    },
+    // nullNameInput: {
+    //   selector: `//DIV[@class='form__validation-message'][text()='Name is required']`,
+    //   locateStrategy: 'xpath',
+    // },
 
-    nullAddressInput: {
-      selector: `//DIV[@class='form__validation-message'][text()='Street is required']`,
-      locateStrategy: 'xpath',
-    },
+    // nullAddressInput: {
+    //   selector: `//DIV[@class='form__validation-message'][text()='Street is required']`,
+    //   locateStrategy: 'xpath',
+    // },
 
-    nullCityInput: {
-      selector: `//DIV[@class='form__validation-message'][text()='City is required']`,
-      locateStrategy: 'xpath',
-    },
+    // nullCityInput: {
+    //   selector: `//DIV[@class='form__validation-message'][text()='City is required']`,
+    //   locateStrategy: 'xpath',
+    // },
 
-    nullStateInput: {
-      selector: `//DIV[@class='form__validation-message'][text()='State is required']`,
-      locateStrategy: 'xpath',
-    },
+    // nullStateInput: {
+    //   selector: `//DIV[@class='form__validation-message'][text()='State is required']`,
+    //   locateStrategy: 'xpath',
+    // },
 
-    nullZipInput: {
-      selector: `//DIV[@class='form__validation-message'][text()='Zip is required']`,
-      locateStrategy: 'xpath',
-    },
+    // nullZipInput: {
+    //   selector: `//DIV[@class='form__validation-message'][text()='Zip is required']`,
+    //   locateStrategy: 'xpath',
+    // },
 
-    alertBox: {
-      selector: `//DIV[@class='alert u-m-b alert--danger']`,
-      locateStrategy: 'xpath',
-    },
+    // alertBox: {
+    //   selector: `//DIV[@class='alert u-m-b alert--danger']`,
+    //   locateStrategy: 'xpath',
+    // },
 
     /*----------------------------------------------------------------------------*/
 

b04005d9a635576fcd16f8e530b9ac5f6ba0b9bd
Keaton Foster <tkfoster26@gmail.com>
2018-07-11 16:23:02 -0400

    moved custom-commands, removed some comments / added some, added tags.js, moved universalElements to universal group folder

--- nightwatch_tests/tests/canary/endUserMessaging.js
+++ nightwatch_tests/tests/canary/endUserMessaging.js
@@ -1,6 +1,7 @@
 /*
 Requirements to pass:
   1. Kvothe Kingkiller (patient) must be assigned to Keaton Tester (member)
+  2. Kvothe Kingkiller is a signed-up patient
 */
 
 const helpers = require('../../helpers');
@@ -8,18 +9,8 @@ let num = helpers.randoNum;
 let messageContent = `Just a test ${num}`;
 
 module.exports = {
-  /*
-    Nightwatch passes in the "client" object into each test file. This object contains all of
-    the Page Objects. These "page objects" contain the functions we'll want to use for each test we write. 
-    Generally speaking, we always start at the Login page.
-  */
   'Login as a patient': function(client) {
-    // this login object contains functions for any action we would want to
-    // take, also any elements we want to interact with, on the Login page.
     const login = client.page.LoginPage();
-
-    // navigate() and submit() are native Nightwatch functions, where as enterPatientCreds() is one we wrote ourselves in the LoginPage.js page object.
-    // PageObjects will always have access to both. 
     
     login.navigate()
       .enterPatientCreds('kvothe', 'Kingkiller1!')

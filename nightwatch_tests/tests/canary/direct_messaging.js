9bf4193ca658b6b99d77d4cf95660e4306206216
Keaton Foster <tkfoster26@gmail.com>
2018-08-06 10:49:15 -0400

    broke tests into separate tests

--- nightwatch_tests/tests/canary/direct_messaging.js
+++ nightwatch_tests/tests/canary/direct_messaging.js
@@ -1,10 +1,9 @@
 /*
-This test uses Secure Messaging to test inbound/outbound messaging between a member and an end user (patient). A message is sent from patient -> member. The member logs in and verifies that the message came through. Afterwards, the member sends a Direct Message to another member, who then logs in and verifies that message's successful arrival.
+This test tests Direct Messaging between two members. A message is sent from member -> member. The second member logs in and verifies that the message came through.
 
 Preconditions/Credentials used:
-  1. kvothe / Kingkiller1! (patient)
-  2. nightkeaton / Chacoz123 (member)
-  3. chatty / Chatty123 (member)
+  1. nightkeaton / Chacoz123 (member)
+  2. chatty / Chatty123 (member)
 */
 
 const helpers = require('../../helpers');
@@ -12,35 +11,12 @@ let num = helpers.randoNum;
 let messageContent = `Just a test ${num}`;
 
 module.exports = {
-  'Login as a patient': function(client) {
-    const login = client.page.LoginPage();
-    
-    login.navigate()
-      .enterPatientCreds('kvothe', 'Kingkiller1!')
-      .submit();
-  },
-
-  'Send a message as a patient': function(client) {
-    const endUserThread = client.page.EUThreadPage();
-    
-    endUserThread.fillInMessageInput(messageContent)
-      .pause(1000) // waiting for Send button to activate
-      .clickSend()
-      .clickSettingsDropdown()
-      .clickLogoutButton();
-  },
 
   'Login as a member': function(client) {
     const login = client.page.LoginPage();
 
     login.enterMemberCreds('nightkeaton', 'Chacoz123')
       .submit();
-  },
-
-  'Find that thread and view the message from the patient': function(client) {
-    const inbox = client.page.DirectInboxPage();
-
-    helpers.findTextOnPage(inbox, messageContent);
   },
 
   'Go to Chatty Members direct chat thread': function(client) {

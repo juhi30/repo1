1a02b52ab7f0665b452c74506961372d66ad066f
Keaton Foster <tkfoster26@gmail.com>
2018-08-17 11:53:05 -0400

    not sure'

--- nightwatch_tests/page_objects/TagsPage.js
+++ nightwatch_tests/page_objects/TagsPage.js
@@ -51,6 +51,24 @@ const tagCommands = {
       .waitForElementNotVisible('@updateTagButton', 'Edit Tag Modal is hidden')
       .waitForElementNotPresent('@editedTag', 'Tag is deleted')
   },
+
+  tagContainerCheck: function () {
+    return this.waitForElementPresent('@newTagContainerButton', 'Tag container is visible')
+  },
+
+  clickToToggleTag: function () {
+    return this.waitForElementPresent('@youreItTag', 'youreIt_tag is visible')
+      .click('@youreItTag')
+  },
+
+  checkTagSelected: function() {
+    return this.verify.cssProperty('@youreItTag', 'color', 'rgba(0, 117, 201, 1)', 'Tag is selected and highlighted')
+  },
+
+  checkTagDeselected: function() {
+    return this.verify.cssProperty('@youreItTag', 'color', 'rgba(64, 64, 64, 1)', 'Tag is deselected')
+
+  },
 }
 
 module.exports = {
@@ -65,23 +83,20 @@ module.exports = {
     /*------------------------------------------------------------*/
 
     newTagButton: {
-      selector: `//BUTTON[contains(@title, 'Create Tag')]`,
+      selector: `//BUTTON[contains(@title, 'Create Tag)]`,
+      locateStrategy: 'xpath',
+    },
+
+    firstTag: {
+      selector: `//SPAN[contains(.,'#Charleston')]`, 
       locateStrategy: 'xpath'
     },
 
-    // a tag that says "Charleston"...
-    charlestonTag: {
-      selector: `//SPAN[contains(.,'#Charleston')]`,
-      locateStrategy: 'xpath'
-    },
-
-    // a tag that says "fake_tag"...
     fakeTag: {
       selector: `//SPAN[contains(.,'#fake_tag')]`,
       locateStrategy: 'xpath'
     },
 
-    // a tag that says "Edited_tag"...
     editedTag: {
       selector: `//SPAN[contains(.,'#Edited_tag')]`,
       locateStrategy: 'xpath'
@@ -138,6 +153,20 @@ module.exports = {
     deleteTagConfirmButton: {
       selector: `//SPAN[contains(text(),'Yes, delete tag')]`,
       locateStrategy: 'xpath',
+    },
+
+    /*------------------------------------------------------------*/
+    // xpaths for assigned tags
+    /*------------------------------------------------------------*/
+    
+    newTagContainerButton: {
+      selector: `//SPAN[contains(text(),'Create New Tag')]`,
+      locateStrategy: 'xpath'
+    },
+
+    youreItTag: {
+      selector: `//SPAN[contains(.,'#youreIt_tag')]`,
+      locateStrategy: 'xpath'
     },
 
   }

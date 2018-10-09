2350a801b0dba886abe398efa217814c9b930115
geoffmaas <geoff@rhinogram.com>
2018-10-09 15:12:13 -0400

    added nightwatch reporter to a slack channel

--- nightwatch_tests/globals.js
+++ nightwatch_tests/globals.js
@@ -1,11 +1,16 @@
-var HtmlReporter = require('nightwatch-html-reporter');
-var reporter = new HtmlReporter({
-    openBrowser: true,
-    reportsDirectory: __dirname + '/reports'
-});
+options = {
+    slack_message: function (results, options) { // function or message string
+        return {
+            text: 'Test completed, passed ' + results.passed + ', failed ' + results.failed,
+            username: 'Nightwatch',
+            icon_emoji: ':ghost:'
+        } // Message payload or string
+    },
+    slack_webhook_url: process.env.SLACK_WEBHOOK_URL
+}
 
 module.exports = {
-    reporter: reporter.fn,
+    reporter: (require('nightwatch-slack-reporter')(options)),
     abortOnAssertionFailure: false,
     waitForConditionPollInterval: 300,
     waitForConditionTimeout: 10000,

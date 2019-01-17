b50e585a84cfcaaf38a24ee58699ab2ba77c2ed4
Mahima Agrawal <mahi9.goyal@gmail.com>
2019-01-17 17:45:41 +0530

    Remove nigtwatch congig and package json changes

--- nightwatch_tests/nightwatch.conf.js
+++ nightwatch_tests/nightwatch.conf.js
@@ -15,34 +15,35 @@ const headless = !!process.env.HEADLESS && process.env.HEADLESS === 'true';
 const options = headless ? { "args": ["headless", "no-sandbox", "disable-gpu"] } : undefined;
 
 module.exports = {
-  "src_folders" : ["tests"],
-  "output_folder" : "reports",
-  "page_objects_path" : "page_objects",
-  "globals_path" : "globals.js",
+  "src_folders": ["tests"],
+  "output_folder": "reports",
+  "page_objects_path": "page_objects",
+  "globals_path": "globals.js",
   "output_folder": "./reports",
 
-  "selenium" : {
-    "start_process" : true,
-    "server_path" : "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.141.5.jar",
-    "log_path" : "",
-    "port" : process.env.PORT || 4444,
-    "cli_args" : {
-      "webdriver.chrome.driver" : "./node_modules/chromedriver/bin/chromedriver",
+  "selenium": {
+    "start_process": true,
+    "server_path": "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.8.1.jar",
+    "log_path": "",
+    "port": process.env.PORT || 4444,
+    "cli_args": {
+      "webdriver.chrome.driver": "./node_modules/chromedriver/bin/chromedriver",
       "webdriver.gecko.driver": "./node_modules/geckodriver/bin/geckodriver"
     }
   },
 
-  "test_settings" : {
-    "default" : {
-      "launch_url" : process.env.LAUNCH_URL || "http://localhost:3000",
-      "selenium_port"  : process.env.PORT || 4444,
-      "selenium_host"  : process.env.SELENIUM_HOST || "127.0.0.1",
-      "silent" : true,
+  "test_settings": {
+    "default": {
+      "launch_url": process.env.LAUNCH_URL || "http://localhost:3000",
+      "selenium_port": process.env.PORT || 4444,
+      "selenium_host": process.env.SELENIUM_HOST || "127.0.0.1",
       "desiredCapabilities": {
         "browserName": process.env.BROWSER_NAME || "chrome",
         "javascriptEnabled": true,
         "acceptSslCerts": true,
-        "chromeOptions": { "args": [ "window-size=1280,800" ] }
+        "chromeOptions": {
+          "args": ["headless", "no-sandbox", "disable-gpu"]
+        }
       }
     }
   }

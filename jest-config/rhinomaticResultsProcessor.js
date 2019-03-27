20ee91b3430cf04c6569701fe04d24a41a36863a
heena-choudhary <heenachoudhary89@gmail.com>
2019-03-27 05:37:40 -0400

    removed circleci changes

--- jest-config/rhinomaticResultsProcessor.js
+++ jest-config/rhinomaticResultsProcessor.js
@@ -1,5 +1,6 @@
 function processTestResults(results){
-  if(results.numFailedTests != 0){
+  console.log(results)
+  if(results.numFailedTests != 0 || results.numFailedTestSuites !=0){
     process.exit(1);
   }
   return results;

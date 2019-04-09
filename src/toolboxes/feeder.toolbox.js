d6968a8fc72ceb5ef64a6486e00d9e87bbe4971d
heena-choudhary <heenachoudhary89@gmail.com>
2019-04-09 08:19:30 -0400

    added missing feeder data

--- src/toolboxes/feeder.toolbox.js
+++ src/toolboxes/feeder.toolbox.js
@@ -49,4 +49,18 @@ module.exports = {
     newChannelName : 'Automation Test Channel New',
     newPurpose: 'New change in purpose',
     rhinoChannelNewName : 'New RhinoSecure Channel',
+
+    //Tags
+    tagNameNewPhoneType: 'Test_1',
+    tagNameRhinoType: 'Test_2',
+
+    //Web Form Addition Details
+   formTitleName: 'Send Message via web form.',
+   titleSubtext: 'Text or Call us.',
+   phonePlaceholder: 'Enter Phone Number',
+   phoneHelpText: 'This will be used in response to question.',
+   messagePlaceHolder: 'Enter your question here.',
+   submitButton: 'Do send your message.',
+   callToActionButton: 'Send Message',
+   confirmationText: 'The message has been submitted successfully!',
 };

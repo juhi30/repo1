648b7ad22cdd8af4c2387c0ec018b9d1d6bdbc1c
Ben Bruning <bruning@gmail.com>
2019-04-12 15:24:12 -0400

    clean

--- src/services/Rhinoapi.service.js
+++ src/services/Rhinoapi.service.js
@@ -8,7 +8,6 @@ export async function findUserByUser(orgId, user) {
 }
 
 export async function getUserByExternalId(orgId, externalId) {
-  axios.defaults.headers.common.Authorization = `Basic ${Buffer.from(process.env.API_BASIC_AUTH).toString('base64')}`;
   return axios.post(`${process.env.API_BASE_URL}/rhinoliner/users/matching`, { orgId, user: { externalId } },
     { headers: { Authorization: `Basic ${Buffer.from(process.env.API_BASIC_AUTH).toString('base64')}` }});
 }

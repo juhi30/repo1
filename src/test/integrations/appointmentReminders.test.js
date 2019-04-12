7235ad8c867378ebe01b25afefbdcf26ab1f5dff
Ben Bruning <bruning@gmail.com>
2019-04-11 20:32:18 -0400

    additional tests

--- src/test/integrations/appointmentReminders.test.js
+++ src/test/integrations/appointmentReminders.test.js
@@ -14,6 +14,8 @@ const USER_TYPE_PATIENT = 18;
 
 let createdPatient;
 let createdAppointment;
+let createdAppointment1;
+let createdAppointment2;
 
 const orgId = process.env.EXISTING_ORG_ID;
 const patientExternalId = process.env.APPOINTMENT_PATIENT_EXTERNAL_ID;
@@ -169,12 +171,22 @@ describe('appointment reminder tests', () => {
     });
   });
 
-  test('find appointment', async (done) => {
+  test('find appointments', async (done) => {
     jest.setTimeout(30000);
     await sleep(10000);
-    rhinoapi.getApointmentByExternalId(orgId, appointmentExternalId, createdPatient.id).then((response) => {
+    await rhinoapi.getApointmentByExternalId(orgId, appointmentExternalId, createdPatient.id).then((response) => {
       expect(response.data.externalId).toBe(appointmentExternalId);
       createdAppointment = response.data;
+    });
+
+    await rhinoapi.getApointmentByExternalId(orgId, `${appointmentExternalId}1`, createdPatient.id).then((response) => {
+      expect(response.data.externalId).toBe(`${appointmentExternalId}1`);
+      createdAppointment1 = response.data;
+    });
+
+    await rhinoapi.getApointmentByExternalId(orgId, `${appointmentExternalId}2`, createdPatient.id).then((response) => {
+      expect(response.data.externalId).toBe(`${appointmentExternalId}2`);
+      createdAppointment2 = response.data;
       done();
     });
   });
@@ -187,7 +199,7 @@ describe('appointment reminder tests', () => {
     });
   });
 
-  test('send appointment reminder message', (done) => {
+  test('send appointment reminder message with confirm/cancel', (done) => {
     jest.setTimeout(30000);
 
     const message = {
@@ -287,6 +299,138 @@ describe('appointment reminder tests', () => {
       expect(response.data.userId).toBe(createdPatient.id);
       expect(response.data.appointmentStatusTypeId).toBe(83); // cancelled
       expect(response.data.appointmentStatusUpdatedByTypeId).toBe(89); // codified
+      done();
+    });
+  });
+
+  test('send appointment reminder message with confirm only', async (done) => {
+    jest.setTimeout(30000);
+
+    const message = {
+      userId: createdPatient.id,
+      appointmentId: createdAppointment1.id,
+      channelId: Number(process.env.EXISTING_ORG_CHANNEL_ID),
+      messageText: 'Outgoing reminder test',
+      phoneId: createdPatient.phones[0].id,
+      phoneNumber: createdPatient.phones[0].number,
+      appointmentEventTypeId: 65, // reminder
+      appointmentReminderResponseTypeId: 78, // confirm only
+    };
+
+    rhinoapi.postAppointmentReminderMessage(message).then((response) => {
+      done();
+    });
+  });
+
+  test('send incoming cancellation text to confirm only appointment', async (done) => {
+    jest.setTimeout(30000);
+    const message = {
+      to: process.env.TEST_BANDWIDTH_NUMBER_ORG,
+      from: createdPatient.phones[0].number,
+      media: [],
+      text: '2',
+      messageId: '7f47f4bf-390d-4d5f-b1a9-7db5eade2464'
+    }
+
+    // reset to uncomfirmed
+    await rhinoapi.updateAppointment(createdAppointment1.id, { appointmentStatusTypeId: 81 });
+
+    await rhinoapi.postIncomingBandwidthMessage(message);
+
+    await sleep(10000);
+
+    rhinoapi.getApointmentByExternalId(orgId, createdAppointment1.externalId, createdPatient.id).then((response) => {
+      expect(response.data.externalId).toBe(createdAppointment1.externalId);
+      expect(response.data.userId).toBe(createdPatient.id);
+      expect(response.data.appointmentStatusTypeId).toBe(81); // unconfirmed
+      done();
+    });
+  });
+
+  test('send incoming confirmation text to confirm only appointment', async (done) => {
+    jest.setTimeout(30000);
+    const message = {
+      to: process.env.TEST_BANDWIDTH_NUMBER_ORG,
+      from: createdPatient.phones[0].number,
+      media: [],
+      text: '1',
+      messageId: '7f47f4bf-390d-4d5f-b1a9-7db5eade2464'
+    }
+
+    await rhinoapi.postIncomingBandwidthMessage(message);
+
+    await sleep(10000);
+
+    rhinoapi.getApointmentByExternalId(orgId, createdAppointment1.externalId, createdPatient.id).then((response) => {
+      expect(response.data.externalId).toBe(createdAppointment1.externalId);
+      expect(response.data.userId).toBe(createdPatient.id);
+      expect(response.data.appointmentStatusTypeId).toBe(82); // confirmed
+      done();
+    });
+  });
+
+  test('send appointment reminder message with cancel only', (done) => {
+    jest.setTimeout(30000);
+
+    const message = {
+      userId: createdPatient.id,
+      appointmentId: createdAppointment2.id,
+      channelId: Number(process.env.EXISTING_ORG_CHANNEL_ID),
+      messageText: 'Outgoing reminder test',
+      phoneId: createdPatient.phones[0].id,
+      phoneNumber: createdPatient.phones[0].number,
+      appointmentEventTypeId: 65, // reminder
+      appointmentReminderResponseTypeId: 79, // cancel only
+    };
+
+    rhinoapi.postAppointmentReminderMessage(message).then((response) => {
+      done();
+    });
+  });
+
+  test('send incoming confirmation text to cancel only appointment', async (done) => {
+    jest.setTimeout(30000);
+    const message = {
+      to: process.env.TEST_BANDWIDTH_NUMBER_ORG,
+      from: createdPatient.phones[0].number,
+      media: [],
+      text: '1',
+      messageId: '7f47f4bf-390d-4d5f-b1a9-7db5eade2464'
+    }
+
+    // reset to uncomfirmed
+    await rhinoapi.updateAppointment(createdAppointment2.id, { appointmentStatusTypeId: 81 });
+
+    await rhinoapi.postIncomingBandwidthMessage(message);
+
+    await sleep(10000);
+
+    rhinoapi.getApointmentByExternalId(orgId, createdAppointment2.externalId, createdPatient.id).then((response) => {
+      expect(response.data.externalId).toBe(createdAppointment2.externalId);
+      expect(response.data.userId).toBe(createdPatient.id);
+      expect(response.data.appointmentStatusTypeId).toBe(81); // unconfirmed
+      done();
+    });
+  });
+
+  test('send incoming cancellation text to cancel only appointment', async (done) => {
+    jest.setTimeout(30000);
+    const message = {
+      to: process.env.TEST_BANDWIDTH_NUMBER_ORG,
+      from: createdPatient.phones[0].number,
+      media: [],
+      text: '2',
+      messageId: '7f47f4bf-390d-4d5f-b1a9-7db5eade2464'
+    }
+
+    await rhinoapi.postIncomingBandwidthMessage(message);
+
+    await sleep(10000);
+
+    rhinoapi.getApointmentByExternalId(orgId, createdAppointment2.externalId, createdPatient.id).then((response) => {
+      expect(response.data.externalId).toBe(createdAppointment2.externalId);
+      expect(response.data.userId).toBe(createdPatient.id);
+      expect(response.data.appointmentStatusTypeId).toBe(83); // cancelled
       done();
     });
   });

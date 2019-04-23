201f88f9b8b39352121da6ea77efe34862e84ed0
ankita singh <ankita.singh@tothenew.com>
2019-04-23 16:49:47 +0530

    final work for RHIN-2225 after the pull from the develop

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -9,8 +9,7 @@ const options = headless ? { "args": ["headless", "no-sandbox", "disable-gpu"] }
 
 module.exports = {
   "page_objects_path" : "./src/page_objects",
-  "globals_path": "./src/globals.js",
-
+  
   "selenium" : {
     "start_process" : true,
     "server_path" : "./node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.141.5.jar",

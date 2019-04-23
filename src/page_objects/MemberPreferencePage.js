85b8b7fd861eaea7dc10fff4fd2101166f9c729b
Sonam <sonam@Sonams-MacBook-Pro.local>
2019-04-23 11:33:20 +0530

    RHIN-2226: Remove extra space

--- src/page_objects/MemberPreferencePage.js
+++ src/page_objects/MemberPreferencePage.js
@@ -1,6 +1,6 @@
 const testConstants = require('../toolboxes/feeder.toolbox')
 const memberPreferencesCommands = {
-  clickEventOnMemberPreferencesPage: function(element,message) {
+  clickEventOnMemberPreferencesPage: function (element, message) {
     return this.waitForElementVisible(element, message + ' is visible.')
       .click(element)
   }
@@ -13,10 +13,6 @@ module.exports = {
   },
   elements: {
 
-    /*---------------------------------------------------------------*/
-    // Mode of Notifications of  Patient Messages
-    /*---------------------------------------------------------------*/
-
     notificationOnDesktopIfAssign: {
       selector: `//INPUT[contains(@title,"Inbox Assigned to Me Desktop")]/following-sibling::label`,
       locateStrategy: 'xpath',
@@ -28,7 +24,7 @@ module.exports = {
     },
 
     groupNotificationLinkOnPatientMsgs: {
-      selector:`//th[text()= 'Patient']//parent::tr//parent::thead//parent::table//Span[contains(text(), 'Individual Group Notifications') or contains(text(), 'Hide Individual Groups')]`,
+      selector: `//th[text()= 'Patient']//parent::tr//parent::thead//parent::table//Span[contains(text(), 'Individual Group Notifications') or contains(text(), 'Hide Individual Groups')]`,
       locateStrategy: 'xpath',
     },
 
@@ -38,7 +34,7 @@ module.exports = {
     },
 
     groupNotificationLinkOnTeamMsgs: {
-      selector:`//th[text()= 'Team']//parent::tr//parent::thead//parent::table//Span[contains(text(), 'Individual Group Notifications') or contains(text(), 'Hide Individual Groups')]`,
+      selector: `//th[text()= 'Team']//parent::tr//parent::thead//parent::table//Span[contains(text(), 'Individual Group Notifications') or contains(text(), 'Hide Individual Groups')]`,
       locateStrategy: 'xpath',
     },
 
@@ -48,7 +44,7 @@ module.exports = {
     },
 
     groupNotificationLinkOnPatientAndTeamMsgs: {
-      selector:`//th[text()= 'Patient and Team']//parent::tr//parent::thead//parent::table//Span[contains(text(), 'Individual Group Notifications') or contains(text(), 'Hide Individual Groups')]`,
+      selector: `//th[text()= 'Patient and Team']//parent::tr//parent::thead//parent::table//Span[contains(text(), 'Individual Group Notifications') or contains(text(), 'Hide Individual Groups')]`,
       locateStrategy: 'xpath',
     },
 
@@ -58,14 +54,14 @@ module.exports = {
     },
 
     updatePrefrencesButton: {
-      selector:`//BUTTON[contains(@class, 'button')]//span[contains(text(), 'Update Preferences')]`,
+      selector: `//BUTTON[contains(@class, 'button')]//span[contains(text(), 'Update Preferences')]`,
       locateStrategy: 'xpath',
     },
 
     updationSuccessfulMessage: {
-        selector: `//DIV[text()='Preferences updated successfully.']`,
-        locateStrategy: 'xpath',
-      },
+      selector: `//DIV[text()='Preferences updated successfully.']`,
+      locateStrategy: 'xpath',
+    },
 
   }
 };

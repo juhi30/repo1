440508b6b4537b9304e5670e4a423bcd5b355fa1
Ben Bruning <bruning@gmail.com>
2019-04-26 11:44:37 -0400

    cleanup

--- src/test/integrations/orgSetupAndTeardown.js
+++ src/test/integrations/orgSetupAndTeardown.js
@@ -1,39 +1,65 @@
-const testConstants = require('../../toolboxes/feeder.toolbox');
 import {
   createOrganization,
   deleteOrganization,
   archiveOrganization,
-  login
+  login,
 } from '../../services/Rhinoapi.service';
 
+const testConstants = require('../../toolboxes/feeder.toolbox');
+
 // CREATE MY NEW ORG HERE
+// eslint-disable-next-line no-undef
 beforeAll(async () => {
   try {
     const cookie = await login();
+    // eslint-disable-next-line no-console
     console.log(cookie);
-    const orgData = {"name":"Integrations Testing","parentCompany":"","street1":"123 happy lane","street2":"","city":"London","zip":"43140","state":"OH","businessAddress":{"street1":"1120 Val Wilson Rd","street2":"","city":"London","state":"OH","zip":"43140"},"contactName":"","contactPhone":"","contactEmail":"","billingChecked":true,"selectedBillingOpt":"newCust"}
+    const orgData = {
+      name: 'Integrations Testing',
+      parentCompany: '',
+      street1: '123 happy lane',
+      street2: '',
+      city: 'London',
+      zip: '43140',
+      state: 'OH',
+      businessAddress: {
+        street1: '1120 Val Wilson Rd', street2: '', city: 'London', state: 'OH', zip: '43140',
+      },
+      contactName: '',
+      contactPhone: '',
+      contactEmail: '',
+      billingChecked: true,
+      selectedBillingOpt: 'newCust',
+    };
     const org = await createOrganization(orgData, cookie);
     process.env.INTERATIONS_ORG = org;
     process.env.INTEGRATIONS_ORG_ID = org.id;
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log('===error on before all orgSetupAndTeardown=======', err);
+    // eslint-disable-next-line no-undef
     done(err);
   }
 });
 
-// DELETE MY NEW ORG HERE 
+// DELETE MY NEW ORG HERE
+// eslint-disable-next-line no-undef
 afterAll(async (done) => {
-
   try {
+    // eslint-disable-next-line no-console
     console.log('Login...');
     const cookie = await login();
+    // eslint-disable-next-line no-console
     console.log('Deleting Org ==', process.env.INTEGRATIONS_ORG_ID);
     const archiveResponse = await archiveOrganization(process.env.INTEGRATIONS_ORG_ID, cookie);
+    // eslint-disable-next-line no-console
     console.log('======== Organization Archive Response =======', archiveResponse);
-    const deleteResponse = await deleteOrganization(process.env.INTEGRATIONS_ORG_ID, cookie);
+    await deleteOrganization(process.env.INTEGRATIONS_ORG_ID, cookie);
+    // eslint-disable-next-line no-console
     console.log('====== Organization Deleted =======');
     done();
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log('===error on after all orgSetupAndTeardown=======', err);
     done(err);
   }

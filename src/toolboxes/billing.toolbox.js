9a79c314ccef8a4ef68cff86e8958edfb89a25a8
ankita singh <ankita.singh@tothenew.com>
2019-05-13 19:13:19 +0530

    code after re-check

--- src/toolboxes/billing.toolbox.js
+++ src/toolboxes/billing.toolbox.js
@@ -2,6 +2,7 @@ import { client } from 'nightwatch-api';
 
 const orgProfile = client.page.OrgProfilePage();
 
+
 export async function verifyBillingID() {
   await orgProfile
     .waitForElementVisible('@settingsDropdown', 'Setting Visibility')

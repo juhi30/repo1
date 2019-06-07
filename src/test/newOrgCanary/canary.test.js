f79f000f1473e5085763ffa2d8cc26ed2df2b14f
Andrew London <london.andrewd@gmail.com>
2019-06-07 11:01:39 -0400

    woops

--- src/test/newOrgCanary/canary.test.js
+++ src/test/newOrgCanary/canary.test.js
@@ -1,26 +1,26 @@
-// // CCR Flow
-// import './orgSetupAndTeardown';
-// import './member';
-// import './login';
-// import './channels';
-//
-// // //Member Flow
-// import './group';
-// import './office';
-// import './memberPreferences';
-// import './memberProfile';
-// import './contact';
-// import './template';
-// import './tags';
-// import './patient';
-// import './messaging';
-// import './outofoffice';
-// import './threadActions';
-// import './orgPreferences';
+// CCR Flow
+import './orgSetupAndTeardown';
+import './member';
+import './login';
+import './channels';
+
+// //Member Flow
+import './group';
+import './office';
+import './memberPreferences';
+import './memberProfile';
+import './contact';
+import './template';
+import './tags';
+import './patient';
+import './messaging';
+import './outofoffice';
+import './threadActions';
+import './orgPreferences';
 import './rhinopay';
-// import './analytics';
-//
-// // //Member + CCR flow. This should be the last test case
-// import './orgProfile';
-// import './createAppointment';
-// import './appointmentManager';
+import './analytics';
+
+// //Member + CCR flow. This should be the last test case
+import './orgProfile';
+import './createAppointment';
+import './appointmentManager';

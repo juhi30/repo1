667ff383cacf85931ccec3d2e3faff1a5f00f18d
heena-choudhary <heenachoudhary89@gmail.com>
2019-07-02 15:39:05 +0530

    upload photo fixes and logger for appointment dates

--- src/page_objects/ContactsPage.js
+++ src/page_objects/ContactsPage.js
@@ -165,7 +165,7 @@ const contactsCommands = {
       .click('@addPhotoButton')
       .waitForElementNotVisible('@uploadPhotoButton', 'Upload Photo modal is open')
       .pause(2000);
-    await helper.uploadFile(this, 'contact.png');
+    await helper.uploadFile(this, 'contact.png', '@addProfilePhoto');
     return this.pause(3000)
       .click('@doneUploadPhoto')
       .waitForElementNotPresent('@doneUploadPhoto', 'Upload photo modal is hidden')
@@ -753,6 +753,11 @@ module.exports = {
 
     forwardingToggle: {
       selector: '//LABEL[@class=\'rhinoswitcher__label\'][contains(@for,\'forwarding\')]',
+      locateStrategy: 'xpath',
+    },
+
+    addProfilePhoto: {
+      selector: '//INPUT[@id = \'js-upload-avatar\']',
       locateStrategy: 'xpath',
     },
   },

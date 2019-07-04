889d55a4d5a88c5da70d013e071dde6cd3f9aa1c
heena-choudhary <heenachoudhary89@gmail.com>
2019-07-04 17:22:42 +0530

    changes to make usernames unique by adding a randon munber

--- src/test/newOrgCanary/member.js
+++ src/test/newOrgCanary/member.js
@@ -7,9 +7,10 @@ const helper = require('../../toolboxes/helpers.toolbox');
 
 describe('Members Page', () => {
   test('Adding a new Member with Admin Role', async () => {
+    global.newCanaryUserOne = `${memberFeeder.memberUsername}_${helper.randomNumber}`;
     const memberDetails = [{ element: '@memberFirstName', value: memberFeeder.memberFirstName },
       { element: '@memberLastName', value: memberFeeder.memberLastName },
-      { element: '@memberUsername', value: memberFeeder.memberUsername },
+      { element: '@memberUsername', value: global.newCanaryUserOne },
       { element: '@memberEmailAddress', value: memberFeeder.memberEmail }];
     const roles = ['@adminRole', '@memberRole', '@billingAdminRole'];
 
@@ -17,9 +18,10 @@ describe('Members Page', () => {
   });
 
   test('Adding another Member with Admin Role', async () => {
+    global.newCanaryUserTwo = `${memberFeeder.memberUsername2}_${helper.randomNumber}`;
     const memberDetails2 = [{ element: '@memberFirstName', value: memberFeeder.memberFirstName2 },
       { element: '@memberLastName', value: memberFeeder.memberLastName2 },
-      { element: '@memberUsername', value: memberFeeder.memberUsername2 },
+      { element: '@memberUsername', value: global.newCanaryUserTwo },
       { element: '@memberEmailAddress', value: `${memberFeeder.email}+${helper.randomNumber}@gmail.com` }];
     const roles = ['@adminRole', '@memberRole'];
 
@@ -31,11 +33,11 @@ describe('Members Page', () => {
   });
 
   test('Login as second Member with Admin Role', async () => {
-    const { memberUsername2, memberPassword } = memberFeeder;
+    const { memberPassword } = memberFeeder;
     const tempPassword2 = global.NEW_CANARY_MEMBER2_TEMP_PASSWORD;
     const login = client.page.LoginPage();
 
-    await changePasswordUsingTempPassword(memberUsername2, memberPassword, tempPassword2);
+    await changePasswordUsingTempPassword(global.newCanaryUserTwo, memberPassword, tempPassword2);
     // Below lines have been added to by pass confirm email modal
     await login.clickConfirmEmailOnEmailModal()
       .pause(1000);
@@ -46,11 +48,11 @@ describe('Members Page', () => {
   });
 
   test('Login as New Member with Admin Role', async () => {
-    const { memberUsername, memberPassword } = memberFeeder;
+    const { memberPassword } = memberFeeder;
     const tempPassword = global.NEW_CANARY_MEMBER_TEMP_PASSWORD;
     const login = client.page.LoginPage();
 
-    await changePasswordUsingTempPassword(memberUsername, memberPassword, tempPassword);
+    await changePasswordUsingTempPassword(global.newCanaryUserOne, memberPassword, tempPassword);
     // Below lines have been added to by pass confirm email modal
     await login.clickConfirmEmailOnEmailModal()
       .pause(1000);
